25e7aad42e73c6c6bbe71eae3027ecaa53448e9f
martianfields <117538936+MartianFieldsD@users.noreply.github.com>
2022-12-24 01:32:47 +0530

    updated index

--- pages/home/index.js
+++ pages/home/index.js
@@ -5,7 +5,7 @@ import { Modal } from '../../src/components/modal';
 function Home(props) {
     const [openMenu, setOpenMenu] = useState(false);
     return (
-        <div className={`relative w-screen h-screen flex flex-col pt-[70px] sm:pt-0 sm:justify-center items-center bg-borabayLightBrown`} id='home' >
+        <div className={`relative w-screen h-screen flex flex-col sm:pt-0 sm:justify-center items-center bg-borabayLightBrown`} id='home' >
             <div className='absolute w-[60px] h-[45px] top-5 left-5 flex justify-center items-center lg:hidden'>
                 <Image className='w-full h-full' src={require('../../public/icons/Logo.png')} alt='Logo' />
             </div>
@@ -13,7 +13,11 @@ function Home(props) {
                 <Image src={require('../../public/icons/ham-menu.png')} height={25} width={40} alt='ham-menu' />
             </button>
             {openMenu && <Modal onCloseClick={setOpenMenu} />}
-            <div className='w-[70%] sm:w-[400px] pt-[100px] flex flex-col sm:mr-[8%] lg:mr-0 justify-center sm:self-end lg:self-center items-center gap-2'>
+            <div className='w-[70%] sm:w-[400px] pt-[100px] flex flex-col lg:mr-0 justify-center items-center gap-2
+            mobile:mr-[8%] tablet:mr-[12%] airpad:mr-[15%] mobile:self-center tablet:self-center airpad:self-center
+            laptop:self-center desktop:self-center'>
+
+                {/* <div className='w-[100%] sm:w-[400px] pt-[100px] flex flex-col md:mr-[15%] sm:mr-[8%] lg:mr-0 justify-center sm:self-end lg:self-center items-center gap-2'> */}
                 <span className={`text-borabayDarkBrown font-normal text-lg sm:text-xl text-center font-montserrat`}>Experience Beauty In-Depth  at</span>
                 <span className={`text-borabayBrown font-normal text-3xl sm:text-7xl font-higuen`}>Borabay</span>
                 <span className={`text-borabayBrown text-lg sm:text-xl sm:tracking-[.60em] font-hindguntur`}>THE SALON</span>
@@ -23,20 +27,26 @@ function Home(props) {
                     <Image src={require('../../public/icons/down_arrow.png')} height={70} width={70} alt='Down-Arrow' />
                 </div>
             </div>
-            <div className='absolute left-0 bottom-0 h-[70%] w-[50%] hidden sm:flex items-end'>
-                <Image src={require('../../public/icons/green-leafed-girl.png')} height={650} width={370} alt='green-leafed-girl' />
+            <div id="leftImg" className='absolute left-0 bottom-0 mobile:opacity-50 tablet:opacity-50 airpad:opacity-50
+            mobile:top-[0px]'>
+                <Image src={require('../../public/icons/green-leafed-girl.png')}
+
+                    alt='green-leafed-girl' />
                 {/* <img src='../../public/icons/green-leafed-girl.png' style={{ height: '100px', width: '100px' }} alt='green-leafed-girl' /> */}
             </div>
-            <div className='absolute right-0 -bottom-[0px] hidden sm:flex'>
-                <Image src={require('../../public/icons/white-top-girl.png')} height={700} width={370} alt='white-top-girl' />
+            <div id="rightImg" className='absolute right-0 bottom-0 hidden sm:flex mobile:opacity-50 tablet:opacity-50 airpad:opacity-50'>
+                <Image src={require('../../public/icons/white-top-girl.png')} height={700} width={370}
+
+                    alt='white-top-girl' />
             </div>
             {/* <div className='absolute bottom-0 flex sm:hidden'>
                 <Image src={require('../../public/icons/white-top-girl-uncroped.png')} height={300} width={270} alt='white-top-girl-uncroped' />
             </div> */}
-            <div className='absolute right-0 bottom-[0px] top-[0px] lg:hidden opacity-[0.4]'>
-                <Image src={require('../../public/icons/green-leafed-girl.png')} height={865} width={400} alt='green-leafed-girl' />
-                {/* <Image src={require('../../public/icons/white-top-girl.png')} height='700px' width='370px' alt='white-top-girl-uncroped' /> */}
-            </div>
+            {/* <div className='absolute right-0 bottom-[0px] top-[0px] w-[100%] h-[100%] lg:hidden opacity-[0.4]'>
+                <Image src={require('../../public/icons/green-leafed-girl.png')} alt='green-leafed-girl'
+                    fill sizes="(max-width: 768px) 100vw, (max-width: 992px) 75vw, (max-width: 1200px) 50vw, 33vw" />
+                
+            </div> */}
         </div >
     );
 }
